8b8132c6626bf42ee151b47b809e0e802b906fa7
haganz753 <aduhsakit753@gmail.com>
2019-11-03 17:44:16 +0700

    dash dikit

--- application/views/dosen/dosen_dash.php
+++ application/views/dosen/dosen_dash.php
@@ -1,63 +1,27 @@
 <?php $this->load->view('common/header'); ?>
   <div class="content-wrapper">
+    
     <!-- Content Header (Page header) -->
-
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Dash Dosen</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url('mahasiswa/index'); ?>">Home</a></li>
-              <li class="breadcrumb-item active">Dash Dosen</li>
-            </ol>
-          </div>
-        </div>
-      </div><!-- /.container-fluid -->
-    </section>
-    <section class="content-header">
-        <div class="container-fluid">
-                 <div class="col-md-7">
-            <div class="card">
+    <div class="col-md-12">
+    <div class="card bg-primary">
               <div class="card-header">
-                <h3 class="card-title">List Kegiatan</h3>
+                <center><h3 class="card-title">Jabatan : Koordinator</h3></center>
 
-
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                </div>
+                <!-- /.card-tools -->
               </div>
               <!-- /.card-header -->
-              <div class="card-body p-0">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th style="width: 10px">#</th>
-                      <th ">Kegiatan</th>
-                      <th ">Judul</th>
-                      <th style="width: 40px">Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1.</td>
-                      <td>Proyek 3</td>
-                      <td>Sistem Informasi Bajaj Online</td>
-
-                      <td><a href="<?php echo base_url('Dosen/detil_proyek'); ?>"><button id="edit"  type="button" class="btn btn-block btn-success"> Tampilkan</button></td></a>
-                    </tr>
-                     <tr>
-                      <td>2.</td>
-                      <td>SP Proyek 2</td>
-                      <td>Sistem Informasi Komputerisasi</td>
-
-                      <td><a href="<?php echo base_url('Dosen/detil_proyek'); ?>"><button id="edit"  type="button" class="btn btn-block btn-success"> Tampilkan</button></td></a>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+              <center><div class="card-body">
+                Kegiatan : Proyek 3
+              </div></center>
+              <!-- /.card-body -->
             </div>
-          </section>
-  </div>
+        </div>
+
+</div>
          -- /.container-fluid -->
     
 <?php $this->load->view('common/footer');?>
